6f87fcb0c1c109c1f9f1f83031c8ff5aabf55cb9
alex11prog <106412250+alex11prog@users.noreply.github.com>
2022-06-20 20:20:50 +0300

    Добавление файлов проекта

---
+++ kr/kr/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace kr
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.nameDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.summDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dateofoperationDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.expensesoperationBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.pdbDataSet = new kr.pdbDataSet();
+            this.expenses_operationTableAdapter = new kr.pdbDataSetTableAdapters.expenses_operationTableAdapter();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button_calc = new System.Windows.Forms.Button();
+            this.label_calc = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewHB = new System.Windows.Forms.DataGridView();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button_load_hibernate = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.expensesoperationBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pdbDataSet)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHB)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoGenerateColumns = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.nameDataGridViewTextBoxColumn,
+            this.summDataGridViewTextBoxColumn,
+            this.dateofoperationDataGridViewTextBoxColumn});
+            this.dataGridView1.DataSource = this.expensesoperationBindingSource;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 37);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(465, 564);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // nameDataGridViewTextBoxColumn
+            // 
+            this.nameDataGridViewTextBoxColumn.DataPropertyName = "name";
+            this.nameDataGridViewTextBoxColumn.HeaderText = "Наименование";
+            this.nameDataGridViewTextBoxColumn.Name = "nameDataGridViewTextBoxColumn";
+            this.nameDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // summDataGridViewTextBoxColumn
+            // 
+            this.summDataGridViewTextBoxColumn.DataPropertyName = "summ";
+            this.summDataGridViewTextBoxColumn.HeaderText = "Сумма";
+            this.summDataGridViewTextBoxColumn.Name = "summDataGridViewTextBoxColumn";
+            this.summDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // dateofoperationDataGridViewTextBoxColumn
+            // 
+            this.dateofoperationDataGridViewTextBoxColumn.DataPropertyName = "date_of_operation";
+            this.dateofoperationDataGridViewTextBoxColumn.HeaderText = "Дата";
+            this.dateofoperationDataGridViewTextBoxColumn.Name = "dateofoperationDataGridViewTextBoxColumn";
+            this.dateofoperationDataGridViewTextBoxColumn.ReadOnly = true;
+            // 
+            // expensesoperationBindingSource
+            // 
+            this.expensesoperationBindingSource.DataMember = "expenses_operation";
+            this.expensesoperationBindingSource.DataSource = this.pdbDataSet;
+            // 
+            // pdbDataSet
+            // 
+            this.pdbDataSet.DataSetName = "pdbDataSet";
+            this.pdbDataSet.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // expenses_operationTableAdapter
+            // 
+            this.expenses_operationTableAdapter.ClearBeforeFill = true;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(0, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 4;
+            // 
+            // button_calc
+            // 
+            this.button_calc.Location = new System.Drawing.Point(375, 629);
+            this.button_calc.Name = "button_calc";
+            this.button_calc.Size = new System.Drawing.Size(102, 23);
+            this.button_calc.TabIndex = 2;
+            this.button_calc.Text = "Посчитать";
+            this.button_calc.UseVisualStyleBackColor = true;
+            this.button_calc.Click += new System.EventHandler(this.button_calc_Click);
+            // 
+            // label_calc
+            // 
+            this.label_calc.AutoSize = true;
+            this.label_calc.Location = new System.Drawing.Point(12, 629);
+            this.label_calc.Name = "label_calc";
+            this.label_calc.Size = new System.Drawing.Size(166, 17);
+            this.label_calc.TabIndex = 3;
+            this.label_calc.Text = "Общая сумма расходов:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(468, 17);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Таблица расходов с подключением источника данных в DataGridView";
+            // 
+            // dataGridViewHB
+            // 
+            this.dataGridViewHB.AllowUserToAddRows = false;
+            this.dataGridViewHB.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewHB.Location = new System.Drawing.Point(580, 37);
+            this.dataGridViewHB.Name = "dataGridViewHB";
+            this.dataGridViewHB.RowTemplate.Height = 24;
+            this.dataGridViewHB.Size = new System.Drawing.Size(457, 564);
+            this.dataGridViewHB.TabIndex = 6;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(671, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(268, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Таблица категорий(маппинг  hibernate)";
+            // 
+            // button_load_hibernate
+            // 
+            this.button_load_hibernate.Location = new System.Drawing.Point(940, 629);
+            this.button_load_hibernate.Name = "button_load_hibernate";
+            this.button_load_hibernate.Size = new System.Drawing.Size(97, 23);
+            this.button_load_hibernate.TabIndex = 8;
+            this.button_load_hibernate.Text = "Загрузить";
+            this.button_load_hibernate.UseVisualStyleBackColor = true;
+            this.button_load_hibernate.Click += new System.EventHandler(this.button_load_hibernate_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1078, 667);
+            this.Controls.Add(this.button_load_hibernate);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.dataGridViewHB);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label_calc);
+            this.Controls.Add(this.button_calc);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.expensesoperationBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pdbDataSet)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewHB)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private pdbDataSet pdbDataSet;
+        private System.Windows.Forms.BindingSource expensesoperationBindingSource;
+        private pdbDataSetTableAdapters.expenses_operationTableAdapter expenses_operationTableAdapter;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button_calc;
+        private System.Windows.Forms.Label label_calc;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn nameDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn summDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dateofoperationDataGridViewTextBoxColumn;
+        private System.Windows.Forms.DataGridView dataGridViewHB;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button_load_hibernate;
+    }
+}
+
---
+++ kr/kr/Form1.cs
@@ -0,0 +1,112 @@
+﻿using MySql.Data.MySqlClient;
+using NHibernate;
+using NHibernate.Cfg;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Configuration;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kr
+{
+    public partial class Form1 : Form
+    {
+        
+        private NHibernate.Cfg.Configuration myConfiguration;
+        private ISessionFactory mySessionFactory;
+        private ISession mySession;
+        
+        public struct cat
+        {
+            public int id_cat;
+            public string name;
+            public string descr;
+            public cat(int _id_cat, string _name, string _descr)
+            {
+                id_cat = _id_cat;
+                name = _name;
+                descr = _descr;
+            }
+        }
+        List<cat> cats = new List<cat>();
+
+        private void button_load_hibernate_Click(object sender, EventArgs e)
+        {
+            myConfiguration = new NHibernate.Cfg.Configuration();
+            myConfiguration.Configure();
+            myConfiguration.AddAssembly(typeof(category).Assembly);
+            mySessionFactory = myConfiguration.BuildSessionFactory();
+            mySession = mySessionFactory.OpenSession();
+            using (mySession.BeginTransaction())
+            {
+                ICriteria criteria = mySession.CreateCriteria<category>();
+                IList<category> list = criteria.List<category>();
+                StringBuilder messageString = new StringBuilder(); 
+                cats.Clear();
+                foreach (category category in list)
+                {
+                    cats.Add(new cat(category.id_cat, category.name, category.descr));
+                }
+            }
+            DataTable HB_dt = new DataTable();
+            HB_dt.Columns.Add("Идентификатор", typeof(int));
+            HB_dt.Columns.Add("Наименование", typeof(string));
+            HB_dt.Columns.Add("Описание", typeof(string));
+            for (int i = 0; i < cats.Count; i++)
+            {
+                HB_dt.Rows.Add(cats[i].id_cat, cats[i].name, cats[i].descr);
+            }
+            dataGridViewHB.DataSource = HB_dt;
+
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            this.Text = "Учет бюджета";
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // TODO: данная строка кода позволяет загрузить данные в таблицу "pdbDataSet.expenses_operation". При необходимости она может быть перемещена или удалена.
+            this.expenses_operationTableAdapter.Fill(this.pdbDataSet.expenses_operation);
+        }
+
+        private void button_calc_Click(object sender, EventArgs e)
+        {
+            MySqlConnectionStringBuilder mysqlCSB;
+            mysqlCSB = new MySqlConnectionStringBuilder();
+            mysqlCSB.Server = "localhost";
+            mysqlCSB.Database = "pdb";
+            mysqlCSB.UserID = "root";
+            mysqlCSB.Password = "root";
+            DataTable dt = new DataTable();
+
+            string queryString = @"select sum(summ) from expenses_operation";
+            using (MySqlConnection con = new MySqlConnection())
+            {
+                con.ConnectionString = mysqlCSB.ConnectionString;
+                MySqlCommand com = new MySqlCommand(queryString, con);
+                try
+                {
+                    con.Open();
+                    using (MySqlDataReader dr = com.ExecuteReader())
+                    {
+                        if (dr.HasRows) {
+                            dt.Load(dr);
+                            label_calc.Text = "Общая сумма расходов: " + dt.Rows[0]["sum(summ)"].ToString();
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+                con.Close();
+            }
+        }
+    }
+}
